63b9e1b1d1ab62ba3ec20afe5df5f0bc8d70af10
Ashish Desai <desai.ashish76@gmail.com>
2023-04-10 12:08:21 -0400

    created simpleform component within index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -8,16 +8,62 @@ const petsList = [
   { petName: 'Fido', petType: 'dog' },
   { petName: 'Tweetie', petType: 'canary' },
   { petName: 'Goldie', petType: 'fish' },
-]
+];
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState({
+    petName: '',
+    petType: '',
+  });
+
+  const change = (event) => {
+    const {name, value} = event.target;
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    setPets(pets.concat({petName: formValues.petName, petType: formValues.petType}))
+    setFormValues({petName: '', petType: ''})
+  }
+
+  return (
+    <div className='container'>
+      <h1>SimpleForm App</h1>
+      {pets.map((pet, ind) => {
+        return (
+          <div key={ind}>
+            {pet.petName} is a {pet.petType} 
+          </div>
+        )
+      })}
+      <form onSubmit={submit}>
+        <input 
+          type='text' 
+          name='petName' 
+          placeholder='Enter PetName' 
+          value={formValues.petName}
+          onChange={change}
+        />
+        <input 
+          type='text' 
+          name='petType' 
+          placeholder='Enter PetType' 
+          value={formValues.petType}
+          onChange={change}
+        />
+        <input type='submit' value='create a pet' />
+      </form>
+    </div>
+
+  )
 }
 
 render(
   <>
-    <SimpleForm />
-    {/* <App /> */}
+    {/* <SimpleForm /> */}
+    <App />
   </>
   , document.querySelector('#root')
 )
